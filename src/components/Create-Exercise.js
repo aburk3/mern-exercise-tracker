c029f337fab79052a3af0a4d3994c2476bc183b0
aburk3 <aburk3@gmail.com>
2019-09-04 06:40:24 -0500

    Added onSubmit method and bound the onChange handlers to 'this'

--- src/components/Create-Exercise.js
+++ src/components/Create-Exercise.js
@@ -4,6 +4,12 @@ export default class CreateExercise extends Component {
 	constructor(props) {
 		super(props);
 
+		this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeDuration = this.onChangeDuration.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
 		this.state = {
 			username: '',
 			description: '',
@@ -35,6 +41,21 @@ export default class CreateExercise extends Component {
 		this.setState({
 			date: date
 		});
+	}
+
+	onSubmit(e) {
+		e.preventDefault();
+
+		const exercise = {
+			username: this.state.username,
+			description: this.state.description,
+			duration: this.state.duration,
+			date: this.state.date
+		}
+
+		console.log(exercise);
+
+		window.location = '/';
 	}
 
 	render() {
